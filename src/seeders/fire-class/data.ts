081b2f09d3848faa06eed2f6f68b814ba14cf7e5
Jerson Paniagua <diazpany@gmail.com>
2022-02-23 22:55:48 -0300

    add seed data

---
+++ src/seeders/fire-class/data.ts
@@ -0,0 +1,7 @@
+import { CreateFireClassInput } from '../../fire-class/dto/create-fire-class.input';
+
+export const fire_classes: CreateFireClassInput[] = [
+  { name: 'Clase A'},
+  { name: 'Clase B'},
+  { name: 'Clase C'}
+];
---
+++ src/seeders/fire-type/data.ts
@@ -0,0 +1,9 @@
+import { CreateFireTypeInput } from '../../fire-type/dto/create-fire-type.input';
+
+export const fire_types: CreateFireTypeInput[] = [
+  { name: 'Madera'},
+  { name: 'Papel'},
+  { name: 'Tela'},
+  { name: 'Basura'},
+  { name: 'Materiales ordinarios'}
+];
