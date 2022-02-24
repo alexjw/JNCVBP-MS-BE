3ebe1285475981644b2b4ecc107c7400e20d54cb
Jerson Paniagua <diazpany@gmail.com>
2022-02-23 22:56:24 -0300

    improve seeder functions

--- src/seeders/seeder.ts
+++ src/seeders/seeder.ts
@@ -1,36 +1,37 @@
 import { Injectable, Logger } from "@nestjs/common";
 
 import { DutySeederService } from "./duty/duties.services";
+import { FireCauseSeederService } from "./fire-cause/fire-causes.services";
+import { FireClassSeederService } from "./fire-class/fire-classes.services";
+import { FireTypeSeederService } from "./fire-type/fire-types.services";
 import { RankSeederService } from "./rank/ranks.services";
+import { VolunteerSeederService } from "./volunteer/volunteers.services";
 
 @Injectable()
 export class Seeder {
   constructor(
     private readonly logger: Logger,
     private readonly dutyService: DutySeederService,
-    private readonly rankService: RankSeederService
+    private readonly rankService: RankSeederService,
+    private readonly fireCauseService: FireCauseSeederService,
+    private readonly fireClassService: FireClassSeederService,
+    private readonly fireTypeService: FireTypeSeederService,
+    private readonly volunteerService: VolunteerSeederService,
   ) { }
 
   async seed() {
-    await this.duties();
-    await this.ranks();
+    await this.seedService(this.rankService.create(), "Ranks");
+    await this.seedService(this.dutyService.create(), "Duties");
+    await this.seedService(this.fireCauseService.create(), "Fire Causes");
+    await this.seedService(this.fireClassService.create(), "Fire Classes");
+    await this.seedService(this.fireTypeService.create(), "Fire Types");
+    await this.seedService(this.volunteerService.create(), "Volunteers");
   }
 
-  async ranks() {
-    return await Promise.all(this.rankService.create())
+  async seedService(dataModel, serviceName) {
+    return await Promise.all(dataModel)
       .then(completed => {
-        this.logger.debug('Successfuly completed seeding ranks...');
-        Promise.resolve(completed);
-      })
-      .catch(error => {
-        Promise.reject(error);
-      });
-  }
-
-  async duties() {
-    return await Promise.all(this.dutyService.create())
-      .then(completed => {
-        this.logger.debug('Successfuly completed seeding duties...');
+        this.logger.debug(`Successfuly completed seeding ${serviceName}...`);
         Promise.resolve(completed);
       })
       .catch(error => {
