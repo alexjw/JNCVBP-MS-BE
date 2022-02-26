e75affc1722c6aa74d95e09be121ce482d8fce8c
Jerson Paniagua <diazpany@gmail.com>
2022-02-25 22:01:59 -0300

    minor

--- src/seeders/volunteer/volunteers.module.ts
+++ src/seeders/volunteer/volunteers.module.ts
@@ -2,16 +2,15 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 
 // Implemented Rank Actions
-import { VolunteerSchema } from "src/Volunteers/entities/Volunteer.entity";
-import { VolunteersResolver } from "src/Volunteers/Volunteers.resolver";
-import { VolunteersService } from "src/Volunteers/Volunteers.service";
+import { VolunteerSchema } from "src/volunteers/entities/volunteer.entity";
+import { VolunteersResolver } from "src/volunteers/volunteers.resolver";
+import { VolunteersService } from "src/volunteers/volunteers.service";
 
 import { VolunteerSeederService } from "./volunteers.services";
 
-
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Volunteer', schema: VolunteerSchema }])],
+  imports: [MongooseModule.forFeature([{ name: "Volunteer", schema: VolunteerSchema }])],
   providers: [VolunteerSeederService, VolunteersResolver, VolunteersService],
   exports: [VolunteerSeederService],
 })
-export class VolunteerSeederModule { }
+export class VolunteerSeederModule {}
