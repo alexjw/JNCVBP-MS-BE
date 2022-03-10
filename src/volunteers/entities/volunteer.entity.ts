07ed85b042abffc0530ee380b4705cd7e2f3c357
Jerson Paniagua <diazpany@gmail.com>
2022-03-10 19:54:42 -0300

    update volunteer modules

--- src/volunteers/entities/volunteer.entity.ts
+++ src/volunteers/entities/volunteer.entity.ts
@@ -1,28 +1,66 @@
-import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
-import * as mongoose from 'mongoose'
+import { ObjectType, Field } from "@nestjs/graphql";
+import { Prop } from "@nestjs/mongoose";
+import { Document, Schema } from "mongoose";
+import { Rank } from "src/ranks/entities/rank.entity";
 
 @ObjectType()
 export class Volunteer {
-
-  @Field({nullable: true})
+  @Field({ nullable: true })
   id: string;
 
-  @Field({nullable: true})
+  @Field({ nullable: true })
   _id: string;
 
   @Field()
   name: string;
 
+  @Field()
+  code: string;
+
+  @Field({ nullable: true })
+  blood_type: number;
+
+  @Field({ nullable: true })
+  status: number;
+
+  @Field({ nullable: true })
+  address: string;
+
+  @Field()
+  incorporation_date: Date;
+
+  @Field()
+  birth_date: Date;
+
+  // references
+  @Field(() => Rank, { nullable: true })
+  rank?: Rank;
 }
 
-export const VolunteerSchema = new mongoose.Schema({
-  name: String
-}, { timestamps: true });
+export const VolunteerSchema = new Schema(
+  {
+    name: String,
+    code: { type: String, index: true, unique: true },
+    address: String,
+    incorporation_date: Date,
+    blood_type: { type: Number, default: 0 },
+    status: Number,
+    birth_date: Date,
+    rank: {
+      _id: { type: Schema.Types.ObjectId, ref: "Rank" },
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
 
-export class VolunteerModel extends mongoose.Document{
-
-  _id: string;
-
+export class VolunteerModel extends Document {
   name: string;
-
+  code: string;
+  address: string;
+  blood_type: number;
+  status: number;
+  incorporation_date: Date;
+  birth_date: Date;
+  rank: { _id: string };
 }
