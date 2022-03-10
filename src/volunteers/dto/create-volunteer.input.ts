2e02d0a1077d6176347452f5b4dde36d9ea073de
Jerson Paniagua <diazpany@gmail.com>
2022-03-10 19:53:46 -0300

    create/update input for volunteers

--- src/volunteers/dto/create-volunteer.input.ts
+++ src/volunteers/dto/create-volunteer.input.ts
@@ -1,9 +1,32 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Field } from "@nestjs/graphql";
+import { BloodType } from "src/custom_types/blood_type";
+import { OnlyIdTypeInput } from "src/custom_types/only-id.input";
+import { VolunteerStatus } from "src/custom_types/volunteer_status";
 
 @InputType()
 export class CreateVolunteerInput {
-
   @Field()
   name: string;
 
+  @Field()
+  code: string;
+
+  @Field({ nullable: true })
+  address: string;
+
+  @Field({ nullable: true })
+  blood_type: BloodType;
+
+  @Field()
+  status: VolunteerStatus;
+
+  @Field()
+  incorporation_date: Date;
+
+  @Field({ nullable: true })
+  birth_date: Date;
+
+  // references
+  @Field(() => OnlyIdTypeInput)
+  rank: OnlyIdTypeInput;
 }
--- src/volunteers/dto/update-volunteer.input.ts
+++ src/volunteers/dto/update-volunteer.input.ts
@@ -1,10 +1,36 @@
-import { CreateVolunteerInput } from './create-volunteer.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { CreateVolunteerInput } from "./create-volunteer.input";
+import { InputType, Field, Int, PartialType, GraphQLTimestamp } from "@nestjs/graphql";
+import { OnlyIdTypeInput } from "src/custom_types/only-id.input";
+import { BloodType } from "src/custom_types/blood_type";
+import { VolunteerStatus } from "src/custom_types/volunteer_status";
 
 @InputType()
 export class UpdateVolunteerInput extends PartialType(CreateVolunteerInput) {
-
   @Field()
   id: string;
 
+  @Field()
+  name: string;
+
+  @Field()
+  code: string;
+
+  @Field({ nullable: true })
+  address: string;
+
+  @Field()
+  blood_type: BloodType;
+
+  @Field()
+  status: VolunteerStatus;
+
+  @Field()
+  incorporation_date: Date;
+
+  @Field({ nullable: true })
+  birth_date: Date;
+
+  // references
+  @Field(() => OnlyIdTypeInput)
+  rank: OnlyIdTypeInput;
 }
