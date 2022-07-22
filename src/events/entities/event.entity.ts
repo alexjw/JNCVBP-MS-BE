437c53108fad56e8c74132f5c04154295c8474b8
Alex Jines <abel.oalex@gmail.com>
2022-07-21 20:05:38 -0400

    changed user instead of volunteer on event

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -2,6 +2,7 @@ import { ObjectType, Field } from "@nestjs/graphql";
 import { Volunteer } from "../../volunteers/entities/volunteer.entity";
 import { Document, Schema } from "mongoose";
 import { ObjectId } from "mongodb";
+import { User } from "../../users/entities/user.entity";
 
 @ObjectType()
 export class Event {
@@ -15,15 +16,19 @@ export class Event {
   description: string;
 
   // This needs to be changed to User, when implemented
-  @Field(() => Volunteer, { nullable: true })
-  created_by?: Volunteer;
+  @Field(() => User, { nullable: true })
+  created_by?: User;
+
+  // This needs to be changed to User, when implemented
+  @Field({ nullable: true })
+  createdAt: Date;
 }
 
 export const EventSchema = new Schema(
   {
     description: String,
     created_by: {
-      _id: { type: Schema.Types.ObjectId, ref: "Volunteer" },
+      _id: { type: Schema.Types.ObjectId, ref: "User" },
     },
     disabled: { type: Boolean, default: false },
   },
@@ -36,6 +41,7 @@ export class EventModel extends Document {
   description: string;
 
   created_by?: { _id: string };
+  createdAt: Date;
 
   disabled: boolean;
 }
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -4,9 +4,10 @@ import { EventsResolver } from "./events.resolver";
 import { MongooseModule } from "@nestjs/mongoose";
 import { EventSchema } from "./entities/event.entity";
 import { VolunteersModule } from "../volunteers/volunteers.module";
+import { UsersModule } from "../users/users.module";
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: "Event", schema: EventSchema }]), VolunteersModule],
+  imports: [MongooseModule.forFeature([{ name: "Event", schema: EventSchema }]), UsersModule],
   providers: [EventsResolver, EventsService],
   exports: [EventsService],
 })
