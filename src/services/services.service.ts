c45852814167c3de85c5104a966932c928c3b1e7
Alex Jines <abel.oalex@gmail.com>
2024-08-27 21:57:43 -0400

    added pagination to services

--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -4,6 +4,8 @@ import { UpdateServiceInput } from "./dto/update-service.input";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { ServiceModel } from "./entities/service.entity";
+import * as moment from "moment";
+const _ = require("lodash");
 
 @Injectable()
 export class ServicesService {
@@ -15,6 +17,38 @@ export class ServicesService {
 
   findAll(disabled = false) {
     return this.model.find().where({ disabled });
+  }
+
+  findPaginated(limit, offset, sortField, sortOrder, searchText, disabled = false) {
+    const searchIsValidDate = moment(searchText, "DD/MM/YYYY", true).isValid();
+
+    const query = this.model.find().where({ disabled });
+
+    if (searchIsValidDate) {
+      const floorDate = moment.utc(searchText, "DD/MM/YYYY", true);
+      const ceilingDate = moment.utc(searchText, "DD/MM/YYYY", true);
+      ceilingDate.set({ second: 59, minute: 59, hour: 23 });
+      query
+        // Manually customizing search fields
+        .where({ date: { $gte: floorDate, $lte: ceilingDate } });
+    } else if (searchText) {
+      query.where({
+        $or: [
+          { locality: { $regex: searchText, $options: "i" } },
+          { type: { $regex: searchText, $options: "i" } },
+          { address: { $regex: searchText, $options: "i" } },
+          { alerted_by: { $regex: searchText, $options: "i" } },
+        ],
+      });
+    }
+    const countQuery = _.cloneDeep(query);
+    return {
+      items: query
+        .sort((sortOrder === "desc" ? "-" : "") + sortField)
+        .skip(offset)
+        .limit(limit),
+      totalSize: this.model.countDocuments(countQuery),
+    };
   }
 
   findAllBetween(startDate: Date, endDate: Date) {
